17c3c2a479a90a3db9aee1e4658fb61d189bc878
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-08 23:25:47 +0530

    ADD verify roles to check if it is admin

---
+++ server/src/middleware/adminVerification.middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (req.body.role !== "ADMIN") {
+    res.status(403).json({ message: "Admin access only" });
+    return;
+  }
+  next();
+};
