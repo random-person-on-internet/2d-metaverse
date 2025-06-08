5c66a8ad650028bc411fe0771aaa9807f3e7054d
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-08 23:24:40 +0530

    UPDATE use item routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import itemRoutes from "./routes/item.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
 
 app.get("/", (_: Request, res: Response) => {
   res.send("API Running");
