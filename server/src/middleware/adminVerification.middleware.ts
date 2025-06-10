0b9c9ac6d52aa3d9dd73140cb91843c1c92be74c
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-10 15:39:05 +0530

    UPDATE now works according to index.d.ts

--- server/src/middleware/adminVerification.middleware.ts
+++ server/src/middleware/adminVerification.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.body.role !== "ADMIN") {
+  if (req.user?.role !== "ADMIN") {
     res.status(403).json({ message: "Admin access only" });
     return;
   }
