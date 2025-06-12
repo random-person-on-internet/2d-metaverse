e1cd8bbc1851e9ebf5c34c0e8e4b7819acb20ecd
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-12 22:58:30 +0530

    DELETE changing socket setups

--- server/src/sockets/index.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from "socket.io";
-import { connectionHandler } from "./connectionHandler";
-
-export const initSocketServer = (io: Server) => {
-  io.on("connection", (socket) => {
-    console.log(`Socket connected: ${socket.id}`);
-    connectionHandler(io, socket);
-  });
-};
