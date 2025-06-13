f94391e066a29c0e247dfe6ac48dd8bb38d3c506
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-13 12:37:58 +0530

    UPDATE index.ts to use item handlers

--- server/src/ws/index.ts
+++ server/src/ws/index.ts
@@ -1,12 +1,14 @@
 import { Server } from "socket.io";
 import { playerSocketHandler } from "./handlers/player.handler";
 import { connectedPlayers } from "./cache/players";
+import { itemSocketHandler } from "./handlers/items.handler";
 
 export const registerSocketHandlers = (io: Server) => {
   io.on("connection", (socket) => {
     console.log(`Socket connected: ${socket.id}`);
 
     playerSocketHandler(io, socket);
+    itemSocketHandler(io, socket);
 
     socket.on("disconnect", () => {
       const player = connectedPlayers.get(socket.id);
