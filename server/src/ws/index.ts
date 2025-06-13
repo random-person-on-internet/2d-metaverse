ffc1428e5becac22e70b9e3cf8affe44aeacf268
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-13 14:00:06 +0530

    UPDATE index.ts to use room handlers

--- server/src/ws/index.ts
+++ server/src/ws/index.ts
@@ -2,6 +2,10 @@ import { Server } from "socket.io";
 import { playerSocketHandler } from "./handlers/player.handler";
 import { connectedPlayers } from "./cache/players";
 import { itemSocketHandler } from "./handlers/items.handler";
+import {
+  handleRoomDisconnect,
+  roomSocketHandler,
+} from "./handlers/room.handler";
 
 export const registerSocketHandlers = (io: Server) => {
   io.on("connection", (socket) => {
@@ -9,11 +13,16 @@ export const registerSocketHandlers = (io: Server) => {
 
     playerSocketHandler(io, socket);
     itemSocketHandler(io, socket);
+    roomSocketHandler(io, socket);
 
     socket.on("disconnect", () => {
       const player = connectedPlayers.get(socket.id);
       if (player) {
         connectedPlayers.delete(socket.id);
+
+        // clean room
+        handleRoomDisconnect(socket.id, player.userId, io);
+
         socket.broadcast.emit("player:left", {
           userId: player.userId,
           socketId: socket.id,
