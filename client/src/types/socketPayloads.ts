57fd4645bad0eb2ad6b3fb1dc2dbbb67f0f585c2
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-15 18:43:51 +0530

    ADD types for socket payloads

---
+++ client/src/types/socketPayloads.ts
@@ -0,0 +1,39 @@
+// client -> server
+export interface RoomPayload {
+  roomId: string;
+  userId: number;
+}
+
+export interface MovePayload {
+  x: number;
+  y: number;
+  direction: string;
+  userId: number;
+}
+
+// server -> client
+export interface RoomJoinedPayload {
+  userId: number;
+  socketId: string;
+}
+
+export interface RoomLeftPayload {
+  userId: number;
+  socketId: string;
+}
+
+export interface PlayerData {
+  socketId: string;
+  userId: number;
+  x: number;
+  y: number;
+}
+
+export interface PlayerMovedPayload extends MovePayload {
+  socketId: string;
+}
+
+export interface PlayerActedPayload {
+  action: string;
+  socketId: string;
+}
