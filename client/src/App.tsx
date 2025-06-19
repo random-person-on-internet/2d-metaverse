a72e12b5c9dc377248e2d37d14bd3a45b1447e13
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-19 15:41:18 +0530

    UPDATE to handle react-routes-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
+import React from "react";
 import "./App.css";
+import { useUserStore } from "./stores/userStore";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const isLoggedIn = useUserStore((s) => s.isLoggedIn);
+  const token = useUserStore((s) => s.token);
+
+  if (!isLoggedIn || !token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+}
 
 export default function App() {
   return (
-    <>
-      <h1>Starting UI</h1>
-      <p>Just Testing</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        <Route
+          path="/worlds"
+          element={
+            <ProtectedRoute>
+              <div className="text-white p-8">Worlds page</div>{" "}
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
