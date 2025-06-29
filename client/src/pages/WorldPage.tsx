c87e50ed8af5bcaf8fec9e23b953358ff16b283d
random-person-on-internet <vedlakkad05@gmail.com>
2025-06-30 02:00:35 +0530

    UPDATE uses room id 1 instead of default grid

--- client/src/pages/WorldPage.tsx
+++ client/src/pages/WorldPage.tsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from "react";
-import type { PlayerData, PlayerMovedPayload } from "../types/socketPayloads";
-import { socket } from "../ws/socket";
+import type {
+  PlayerData,
+  PlayerLeftPayload,
+  PlayerMovedPayload,
+} from "../types/socketPayloads";
 import { SocketEmitEvents, SocketListenEvents } from "../types/socketEvents";
+import type { Room } from "../types/roomApi.types";
+import { socket } from "../ws/socket";
 import { useUserStore } from "../stores/userStore";
+import { getRoomById } from "../api/roomApi";
 
 export default function WorldPage() {
   const [players, setPlayers] = useState<PlayerData[]>([]);
-  const [currentRoomId, setCurrentRoomId] = useState<string | null>(null);
+  const [room, setRoom] = useState<Room | null>(null);
   const userId = useUserStore((s) => s.user?.id);
 
   useEffect(() => {
-    socket.connect();
+    const connect = async () => {
+      socket.connect();
+      const token = localStorage.getItem("token");
 
-    const roomId = "1";
-    setCurrentRoomId(roomId);
+      console.log("Fetched token:", token);
+      if (!token) {
+        console.error("No token found.");
+        return;
+      }
 
-    socket.emit(SocketEmitEvents.ROOM_JOIN, {
-      roomId: roomId,
-      userId: userId,
-    });
+      const worldRoom = await getRoomById(1, token!);
+      setRoom(worldRoom);
 
-    // listen all users from server
+      socket.emit(SocketEmitEvents.ROOM_JOIN, {
+        roomId: worldRoom.id,
+        userId: userId,
+      });
+    };
+
+    connect();
+
     socket.on(SocketListenEvents.PLAYER_ALL, (playerList: PlayerData[]) => {
       setPlayers(playerList);
     });
 
-    // listen player movement
     socket.on(
       SocketListenEvents.PLAYER_MOVED,
       (payload: PlayerMovedPayload) => {
@@ -39,27 +54,34 @@ export default function WorldPage() {
       }
     );
 
+    socket.on(SocketListenEvents.PLAYER_LEFT, (payload: PlayerLeftPayload) => {
+      setPlayers((prev) => prev.filter((p) => p.userId !== payload.userId));
+    });
+
     // clean on unmount
     return () => {
       socket.disconnect();
+      socket.off(SocketListenEvents.PLAYER_ALL);
+      socket.off(SocketListenEvents.PLAYER_MOVED);
+      socket.off(SocketListenEvents.PLAYER_LEFT);
     };
   }, [userId]);
 
   // move function for testing
   const move = (dx: number, dy: number) => {
-    if (!currentRoomId) return;
+    if (!room) return;
 
     const me = players.find((p) => p.userId === userId);
     if (!me) return;
 
     // me?.x ?? 0 + dx IS NOT (me?.x ?? 0) + dx
     // learnt that after hours, do not forget this future ved
-    const newX = Math.max(0, Math.min(19, me.x + dx));
-    const newY = Math.max(0, Math.min(19, me.y + dy));
+    const newX = Math.max(0, Math.min(room.xlen, me.x + dx));
+    const newY = Math.max(0, Math.min(room.ylen, me.y + dy));
 
     socket.emit(SocketEmitEvents.PLAYER_MOVE, {
       userId: userId,
-      roomId: currentRoomId,
+      roomId: room.id,
       x: newX,
       y: newY,
       direction: "right",
@@ -76,15 +98,25 @@ export default function WorldPage() {
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [players, currentRoomId]);
+  }, [players, room]);
+
+  if (!room) {
+    return <div className="text-while p-4"> Loading world...</div>;
+  }
 
   return (
-    <>
-      <div className="grid grid-cols-20 grid-rows-20 gap-1 p-8 bg-black">
-        {Array.from({ length: 400 }).map((_, index) => {
-          const x = index % 20;
-          const y = Math.floor(index / 20);
-          const playerHere = players.find((p) => p.x === x && p.y == y);
+    <div className="p-8 bg-black">
+      <div
+        className="grid gap-1"
+        style={{
+          gridTemplateColumns: `repeat(${room.xlen}, 1fr)`,
+          gridTemplateRows: `repeat(${room.ylen}, 1fr)`,
+        }}
+      >
+        {Array.from({ length: room.xlen * room.ylen }).map((_, index) => {
+          const x = index % room.xlen;
+          const y = Math.floor(index / room.xlen);
+          const playerHere = players.find((p) => p.x === x && p.y === y);
 
           return (
             <div
@@ -92,10 +124,10 @@ export default function WorldPage() {
               className={`w-6 h-6 border border-gray-600 relative ${
                 playerHere ? "bg-green-500" : "bg-black"
               }`}
-            ></div>
+            />
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
